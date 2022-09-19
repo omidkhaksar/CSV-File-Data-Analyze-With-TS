fc5e95fdbc58bced2d88b5b9d3abcbd3d0dda372
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-09-19 10:42:11 +0430

    CSV File Reader Class Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import fs from 'fs';
+import {CsvFileReader} from './CsvFileReader';
 
-const matches = fs.readFileSync('./football.csv', {
-    encoding: 'utf8',
-}).split('\n')
-    .map((row: string): string[] => {
-        return row.split(',');
-    })
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D',
+}
 let manUnitedWin = 0;
-
-for (let match of matches) {
-    if (match[1]==='Man United' && match[5] ==='H'){manUnitedWin++}
-    else if(match[2]==='Man United' && match[5] ==='A'){manUnitedWin++}
+for (let match of reader.data) {
+    if (match[1]==='Man United' && match[5] ===MatchResult.HomeWin){manUnitedWin++}
+    else if(match[2]==='Man United' && match[5] ===MatchResult.AwayWin){manUnitedWin++}
 }
 console.log(`Man United Won ${manUnitedWin} Matches`)
