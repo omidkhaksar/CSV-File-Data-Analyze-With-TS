b545a85a770a8bed0812058d69501196da421702
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-09-19 10:05:45 +0430

    Simple Test  of Analyze

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,11 @@ const matches = fs.readFileSync('./football.csv', {
         return row.split(',');
     })
 
-console.log(matches)
 
+let manUnitedWin = 0;
+
+for (let match of matches) {
+    if (match[1]==='Man United' && match[5] ==='H'){manUnitedWin++}
+    else if(match[2]==='Man United' && match[5] ==='A'){manUnitedWin++}
+}
+console.log(`Man United Won ${manUnitedWin} Matches`)
