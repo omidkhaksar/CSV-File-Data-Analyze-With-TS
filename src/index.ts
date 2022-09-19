0f49fd55f462dbd5152df9cb2fcca4cbe39bdc13
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-09-19 11:30:21 +0430

    Convert Row String to Correct Row Data Type

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import fs from 'fs';
 import {CsvFileReader} from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
-enum MatchResult {
-    HomeWin = 'H',
-    AwayWin = 'A',
-    Draw = 'D',
-}
 let manUnitedWin = 0;
 for (let match of reader.data) {
     if (match[1]==='Man United' && match[5] ===MatchResult.HomeWin){manUnitedWin++}
