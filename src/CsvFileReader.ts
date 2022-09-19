212d448dfa0a793ed7ad992a01596c5709803c8d
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-09-19 12:01:08 +0430

    Refactor CSVFileReader (with abstract class and Generic)

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,30 +1,21 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-type MatchData = [Date,string,string,number,number,MatchResult,string];
-    
-export class CsvFileReader{
-    data: MatchData[]=[];
-    constructor(public filename: string){}
-    read():void{
+export abstract class CsvFileReader<T>{
+    data: T[] = [];
+
+    constructor(public filename: string) { }
+
+    abstract mapRow(row: string[]): T;
+
+    read(): void {
         this.data = fs.readFileSync(this.filename, {
             encoding: 'utf8',
         })
-        .split('\n')
-        .map((row: string): string[] => {
-            return row.split(',');
-        })
-        .map((row: string[]):MatchData => {
-            return [
-                dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5] as MatchResult, // 'H','A','D'
-                row[6]
-            ];
-        });
+            .split('\n')
+            .map((row: string): string[] => {
+                return row.split(',');
+            })
+            .map(this.mapRow);
     }
+
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
-export const dateStringToDate = (dateString : string): Date =>{
+export const dateStringToDate = (dateString: string): Date => {
     const dateParts = dateString.split('/')
-    .map((value:string):number=>{
-        return parseInt(value);
-    });
-    return new Date(dateParts[2],dateParts[1]-1,dateParts[0]);
+        .map((value: string): number => {
+            return parseInt(value);
+        });
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
 }
